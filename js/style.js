00f48d06a49b93e0196f9cc57589f6fb32cb9286
Matt Lunalover <mlunalover@gmail.com>
2015-09-08 23:07:51 -0400

    about and form

--- js/style.js
+++ js/style.js
@@ -20,11 +20,19 @@ $(document).ready(function() {
 //Resources Toggle
 
 $("#web-button").click(function(){
+    // $( ".grid-item" ).addClass( "book sites" );
     $(".book").toggle();
+    $(".sites").toggle();
 });
 
 $("#books-button").click(function(){
     $(".webcourse").toggle();
+    $(".sites").toggle();
+});
+
+$("#sites-button").click(function(){
+    $(".webcourse").toggle();
+    $(".book").toggle();
 });
 
 
