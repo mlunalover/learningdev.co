58d66b8b1317e7376f4f277ef77bb42779e22ee3
Matt Lunalover <mlunalover@gmail.com>
2015-09-28 20:12:46 -0400

    fixed media query in resource page

--- js/style.js
+++ js/style.js
@@ -9,32 +9,54 @@ $(document).ready(function() {
 
   //Masonry Js
 
-
-    $('.grid').masonry({
+    var masonryOptions = {
       itemSelector: '.grid-item',
       columnWidth: 160,
       "gutter": 6
 
-      //$grid.marsonry('destory');
-      //$grid.masonry({ itemSelector: '.grid-item'});
-    });
 
+      };
+
+    var $grid = $('.grid').masonry( masonryOptions );
+    var isActive = true;
+
+    // var $grid = $('.grid').masonry({
+    //   itemSelector: '.grid-item',
+    //   columnWidth: 160,
+    //   "gutter": 6
+    //
+    // });
+
+    //$grid.marsonry('destory');
+    //$grid.masonry({ itemSelector: '.grid-item'});
 
 
 //Resources Toggle
 
 $("#web-button").click(function(){
-    // $( ".grid-item" ).addClass( "book sites" );
 
-    $(".book").toggle();
-    $(".sites").toggle();
-    // $grid.marsonry('destory');
-    // $grid.masonry({ itemSelector: '.grid'});
+
+  //   if ( isActive ) {
+  //   $grid.masonry('destroy');
+  // } else {
+  //   $grid.masonry( masonryOptions );
+  // }
+  //
+  // isActive = !isActive;
+
+  // $('.grid').masonry('reload');
+
+  $(".book").toggle();
+  $(".sites").toggle();
+
+
+
 });
 
 $("#books-button").click(function(){
     $(".webcourse").toggle();
     $(".sites").toggle();
+
 });
 
 $("#sites-button").click(function(){
@@ -50,11 +72,15 @@ $(".atom_packages").hide();
 $(".slack_teams").hide();
 
 $(".atom").click(function(){
-    $(".atom_packages").toggle();
+    $(".atom_packages").toggle("600", function(){
+
+    });
 });
 
 $(".slack").click(function(){
-    $(".slack_teams").toggle();
+    $(".slack_teams").toggle("600", function(){
+
+    });
 });
 
 
